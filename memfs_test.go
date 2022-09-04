f8876e2e1d7be22e5944a966a21927a877171efb
kris <kris.watts@gravwell.io>
2022-09-04 13:27:05 -0600

    adding a Remove method to the memFS

--- memfs_test.go
+++ memfs_test.go
@@ -81,3 +81,69 @@ func TestMemFS(t *testing.T) {
 		t.Fatalf("write/read top_level_file.txt mismatch %s", diff)
 	}
 }
+
+func TestMemFSremove(t *testing.T) {
+
+	rootFS := New()
+	err := rootFS.MkdirAll("foo/bar/baz", 0777)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var gotPaths []string
+	err = fs.WalkDir(rootFS, ".", func(path string, d fs.DirEntry, err error) error {
+		gotPaths = append(gotPaths, path)
+		if !d.IsDir() {
+			return fmt.Errorf("%s is not a directory", path)
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectPaths := []string{
+		".",
+		"foo",
+		"foo/bar",
+		"foo/bar/baz",
+	}
+
+	if diff := cmp.Diff(expectPaths, gotPaths); diff != "" {
+		t.Fatalf("WalkDir mismatch %s", diff)
+	}
+
+	err = rootFS.WriteFile("foo/bar/baz/buz.txt", []byte("buz"), 0777)
+	if err == nil && errors.Is(err, fs.ErrNotExist) {
+		t.Fatalf("Expected missing directory error but got none")
+	}
+
+	//fire a delete that WILL faila
+	if err := rootFS.Remove("foo/bar/baz/buzz"); err == nil {
+		t.Fatalf("Failed to catch non-existent foo/bar/baz/buzz")
+	}
+
+	//fire one that will succeed
+	if err := rootFS.Remove("foo/bar/baz"); err != nil {
+		t.Fatalf("Failed to delete foo/bar/baz: %v", err)
+	}
+
+	//walk it again
+	gotPaths = nil
+	err = fs.WalkDir(rootFS, ".", func(path string, d fs.DirEntry, err error) error {
+		gotPaths = append(gotPaths, path)
+		if !d.IsDir() {
+			return fmt.Errorf("%s is not a directory", path)
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// check status
+	expectPaths = expectPaths[0:3]
+	if diff := cmp.Diff(expectPaths, gotPaths); diff != "" {
+		t.Fatalf("WalkDir mismatch %s", diff)
+	}
+}
